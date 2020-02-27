1615384a3b0660d24d23582e172283bbf5c00f12
Jorge Perez <50148675+jorgep17@users.noreply.github.com>
2020-02-27 13:45:37 -0500

    change to the rda

--- data-raw/nycdb.R
+++ data-raw/nycdb.R
@@ -1,3 +1,4 @@
 nycdb <- data.table::fread("nycdb.csv",header = T,stringsAsFactors = F)
 nycdb <- stringi::stri_trans_general(nycdb, "latin-ascii")
+nycdb <- as.data.table(x = nycdb)
 usethis::use_data(nycdb, compress = "xz",overwrite = TRUE)
