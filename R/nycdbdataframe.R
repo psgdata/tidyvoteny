ee1b2e5186a6def887298dde24de55c73751b6d4
Madeleine Lohr <madeleine@theparksidegroup.com>
2020-11-18 22:00:07 +0000

    ML: correcting spelling errors

--- R/nycdbdataframe.R
+++ R/nycdbdataframe.R
@@ -1,8 +1,8 @@
-#' @title NYC ED Level election reaults 2013-2019
+#' @title NYC ED Level election results 2013-2019
 #'
 #' @description data frame containing nyc ed level election results
 #'
-#' @format A data frame with wtih 17 columns
+#' @format A data frame with with 17 columns
 #'
 #'
 #'
