55d71b93e9e505aeca4bf2cfe74774431144f065
Madeleine Lohr <madeleine@theparksidegroup.com>
2020-12-08 20:03:43 +0000

    ML: Appending NYCDB with NYC 2020 Presidential Primary results

--- data-raw/nycdb.R
+++ data-raw/nycdb.R
@@ -1,4 +1,5 @@
+# Compress data table
 nycdb <- data.table::fread("nycdb.csv",header = T,stringsAsFactors = F)
 nycdb <- stringi::stri_trans_general(nycdb, "latin-ascii")
-nycdb <- as.data.table(x = nycdb)
+nycdb <- data.table::as.data.table(x = nycdb)
 usethis::use_data(nycdb, compress = "xz",overwrite = TRUE)
