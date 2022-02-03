3c2d2e75ab83452a3d6d5f705739f0dc33cf2673
Jorge Perez <50148675+jorgep17@users.noreply.github.com>
2022-02-02 23:44:18 -0500

    ml

--- data-raw/nycdb.R
+++ data-raw/nycdb.R
@@ -1,5 +1,5 @@
 # Compress data table
-nycdb <- readRDS("~/r_space/tidyvoteny/nycdb.Rds")
+nycdb <- readRDS("~/work_space/r_spaces/tidyvoteny/nycdb.Rds")
 #nycdb <- stringi::stri_trans_general(nycdb, "latin-ascii")
 nycdb <- data.table::as.data.table(x = nycdb)
 usethis::use_data(nycdb, compress = "xz",overwrite = TRUE)
